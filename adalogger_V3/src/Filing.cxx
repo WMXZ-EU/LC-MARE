326d9dd35abfd5403faa8e789198d0326d72f1d4
WMXZ-EU <WMXZ-EU@users.noreply.github.com>
2026-06-13 16:04:55 +0200

    corrected filing.cxx

--- adalogger_V3/src/Filing.cxx
+++ adalogger_V3/src/Filing.cxx
@@ -171,7 +171,7 @@ char * wavHeaderUpdate(int32_t nbytes, int16_t vsens)
   char *wptr=wav_Info_ptr;
   wptr=insertChunk(wptr,"ICRD",datestring);
   //
-  sprintf(infotext,"%s; %4d; %4d; %4d; %4d; %4d; %6d; %3d; %3d; %4d; %3d; %4d; %4d; %4d; %4d.",
+  sprintf(infotext,"%s; %4d; %4d; %4d; %4d; %4d; %6d; %3d; %3d; %4d; %3d; %4d; %4d; %4d; %4d; %s.",
                     uid_strng,t_acq,t_on,t_rep,fsamp/1000,again, vsens,SHIFT,PROC, NBUF_ACQ, MD,
                     h_rec[0],h_rec[1],h_rec[2],h_rec[3],Version);
   wptr=insertChunk(wptr,"IKEY",infotext);
